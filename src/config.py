6787ce22fd11e0717996684720ee116779fbf9ca
alexey_naidiuk <alexeynaidiuk@gmail.com>
2024-04-29 23:20:19 +0300

    moved req id to .env

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
+import os
 from pathlib import Path
 
+import dotenv
+
+dotenv.load_dotenv()
 
 ROOT_FOLDER = Path(__file__).parent.parent
 
@@ -8,5 +12,4 @@ GOOGLE_AUTH_FOLDER = ROOT_FOLDER / 'google_auth'
 GOOGLE_CREDENTIALS = GOOGLE_AUTH_FOLDER / 'credentials.json'
 GOOGLE_USER = GOOGLE_AUTH_FOLDER / 'authorised_user.json'
 
-
-MONOBANK_REQUEST_ID = 'b4b90e5bae8f1e8a0e1c12798bd75b28e88b44f3'
+MONOBANK_REQUEST_ID = os.environ['MONOBANK_REQUEST_ID']
