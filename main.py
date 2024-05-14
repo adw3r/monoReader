f06da9b18f63bd8e4bb359f6774e7448a61cee15
alexey_naidiuk <alexeynaidiuk@gmail.com>
2024-05-14 22:36:13 +0300

    upd on main.py cli args

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import logging
 from datetime import datetime, timedelta
 
+import click
 import gspread_dataframe
 import httpx
 import pandas
@@ -28,8 +30,12 @@ def reformat_columns(df: pandas.DataFrame):
     ]]
 
 
-def main() -> None:
-    print('start')
+@click.command()
+@click.option('-d', '--days', default=1)
+@click.option('--log-level', default='INFO')
+def main(days: int = 31, log_level: str = 'INFO') -> None:
+    logging.basicConfig(level=log_level)
+    logging.info(f'start {days=}')
     account_info: httpx.Response = parsers.MonoApi.get_account_info()
     account_info_schema: schemas.AccountSchema = schemas.AccountSchema(**account_info.json())
     cards: list[schemas.BankCard] = [
@@ -37,7 +43,7 @@ def main() -> None:
         acc.balance > 0 and acc.currencyCode == 980
     ]
     now: datetime = datetime.now()
-    start_of_the_month: datetime = now - timedelta(days=31)
+    start_of_the_month: datetime = now - timedelta(days=days)
     now_in_timestamp: int = round(datetime.timestamp(now))
     start_of_the_month_in_timestamp: int = round(datetime.timestamp(start_of_the_month))
 
@@ -46,19 +52,25 @@ def main() -> None:
         get_card_info: httpx.Response = parsers.MonoApi.get_card_info(
             card.id, start_of_the_month_in_timestamp, now_in_timestamp
         )
+        logging.info(f'{get_card_info=}')
         df = pandas.DataFrame([schemas.Transaction(**t).dict() for t in get_card_info.json()])
         df['card_type'] = card.type
         frames = pandas.concat([frames, df])
 
-    frames['reformatted amount'] = [i / 100 for i in frames['amount']]
-    frames['reformatted time'] = [datetime.fromtimestamp(timestamp).strftime('%d.%m.%Y') for timestamp in
-                                  frames['time']]
+    frames['reformatted amount'] = [(i / 100) * -1 for i in frames['amount']]
+    frames['reformatted time'] = [
+        datetime.fromtimestamp(timestamp).strftime('%d.%m.%Y')
+        for timestamp in frames['time']
+    ]
+
     frames = reformat_columns(frames)
     frames = frames.sort_values('time')
+
     gc = google_api.get_client()
     ws = gc.open_by_url(config.URL_TO_MAIN).worksheet('data')
+    ws.clear()
     gspread_dataframe.set_with_dataframe(ws, frames, allow_formulas=False)
-    print('finish')
+    logging.info('finish')
 
 
 if __name__ == '__main__':
